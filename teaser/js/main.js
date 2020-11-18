3fb64506e75f9b06e92bcaa799a69e7da32defe0
ais2397@gmail.com <ais2397@gmail.com>
2020-11-19 02:41:35 +0530

    Updating sponsors

--- teaser/js/main.js
+++ teaser/js/main.js
@@ -185,3 +185,37 @@
     });
 
 })(jQuery);
+
+    $(document).ready(function() {
+    	var showChar = 450;
+    	var ellipsestext = "...";
+    	var moretext = "Read more";
+    	var lesstext = "Read less";
+    	$('.more').each(function() {
+    		var content = $(this).html();
+
+    		if(content.length > showChar) {
+
+    			var c = content.substr(0, showChar);
+    			var h = content.substr(showChar-1, content.length - showChar);
+
+    			var html = c + '<span class="moreelipses">'+ellipsestext+'</span>&nbsp;<span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">'+moretext+'</a></span>';
+
+    			$(this).html(html);
+    		}
+
+    	});
+
+    	$(".morelink").click(function(){
+    		if($(this).hasClass("less")) {
+    			$(this).removeClass("less");
+    			$(this).html(moretext);
+    		} else {
+    			$(this).addClass("less");
+    			$(this).html(lesstext);
+    		}
+    		$(this).parent().prev().toggle();
+    		$(this).prev().toggle();
+    		return false;
+    	});
+    });
